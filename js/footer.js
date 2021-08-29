a10053c1a44f482537c7b82ae55cc55c45485a3d
Galarist <galamboscristovao@gmail.com>
2021-08-30 01:08:43 +1000

    Adding footer according to the Wireframe

---
+++ js/footer.js
@@ -0,0 +1,68 @@
+document.write (`
+<footer>
+    <h3>&copy; <span>Copyright</span> 2021</h3>
+    <hr>
+    <h3>Sitemap</h3>
+    <div>
+        <ul>
+            <li><a href="./">Home</a></li>
+            <ul>
+                <li>About Us</li>
+                <li>Services</li>
+                <li>Brands</li>
+            </ul>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li><a href="/products.html">Products</a></li>
+            <ul>
+                <li>Ray-Ban</li>
+                <li>Disney Eyewear</li>
+                <li>Oakley</li>
+            </ul>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li><a href="./book.html">Booking</a></li>
+            <ul>
+                <li>Book for an appointment</li>
+            </ul>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li><a href="./blog.html">Blog</a></li>
+            <ul>
+                <li>About the owner and her philosophy</li>
+                <li>Amanda B Cormer</li>
+            </ul>
+        </ul>
+    </div>
+    <div>
+        <ul>
+            <li><a href="./contact.html">Contact</a></li>
+            <ul>
+                <li>Contact Us</li>
+                <li>Opening Hours</li>
+            </ul>
+        </ul>
+    </div>
+    <hr>
+    <h3>
+        <p>
+            <b>Contact Us:</b>
+        </p>
+        <u>
+            Phone Number: 07 3256 7101
+        </u>
+        <br>
+        <u>
+            The Myer Centre, Queen Street Mall, Level 1, Shop number
+            36
+        </u>
+        <a href="./contact.html">Contact</a>
+    </h3>
+</footer>
+`);
