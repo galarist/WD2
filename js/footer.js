129e4e0f0f140199f982c33430b45e66b8e9cb92
Galarist <galamboscristovao@gmail.com>
2021-08-30 01:37:23 +1000

    Fixing footer according to CSS validator

--- js/footer.js
+++ js/footer.js
@@ -5,55 +5,60 @@ document.write (`
     <h3>Sitemap</h3>
     <div>
         <ul>
-            <li><a href="./">Home</a></li>
-            <ul>
-                <li>About Us</li>
-                <li>Services</li>
-                <li>Brands</li>
-            </ul>
+            <li><a href="./">Home</a>
+                <ul>
+                    <li>About Us</li>
+                    <li>Services</li>
+                    <li>Brands</li>
+                </ul>
+            </li>
         </ul>
     </div>
     <div>
         <ul>
-            <li><a href="/products.html">Products</a></li>
-            <ul>
-                <li>Ray-Ban</li>
-                <li>Disney Eyewear</li>
-                <li>Oakley</li>
-            </ul>
+            <li><a href="/products.html">Products</a>
+                <ul>
+                    <li>Ray-Ban</li>
+                    <li>Disney Eyewear</li>
+                    <li>Oakley</li>
+                </ul>
+            </li>
         </ul>
     </div>
     <div>
         <ul>
-            <li><a href="./book.html">Booking</a></li>
-            <ul>
-                <li>Book for an appointment</li>
-            </ul>
+            <li><a href="./book.html">Booking</a>
+                <ul>
+                    <li>Book for an appointment</li>
+                </ul>
+            </li>
         </ul>
     </div>
     <div>
         <ul>
-            <li><a href="./blog.html">Blog</a></li>
-            <ul>
-                <li>About the owner and her philosophy</li>
-                <li>Amanda B Cormer</li>
-            </ul>
+            <li><a href="./blog.html">Blog</a>
+                <ul>
+                    <li>About the owner and her philosophy</li>
+                    <li>Amanda B Cormer</li>
+                </ul>
+            </li>
         </ul>
     </div>
     <div>
         <ul>
-            <li><a href="./contact.html">Contact</a></li>
-            <ul>
-                <li>Contact Us</li>
-                <li>Opening Hours</li>
-            </ul>
+            <li><a href="./contact.html">Contact</a>
+                <ul>
+                    <li>Contact Us</li>
+                    <li>Opening Hours</li>
+                </ul>
+            </li>
         </ul>
     </div>
     <hr>
     <h3>
-        <p>
+        <span>
             <b>Contact Us:</b>
-        </p>
+        </span>
         <u>
             Phone Number: 07 3256 7101
         </u>
